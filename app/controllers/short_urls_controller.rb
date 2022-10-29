105d9e7c4d70ccc19f83bb3c1b9818981853ae33
Marco <marcom4drigal@gmail.com>
2022-10-28 21:31:50 -0600

    Temporal POST method implementation

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -9,6 +9,23 @@ class ShortUrlsController < ApplicationController
   end
 
   def create
+    #short_url = ShortUrl.new(full_url: params[:full_url], title: "")
+    short_url = ShortUrl.new(short_url_params)
+    puts short_url
+
+    if short_url.save
+      render json: short_url, status: :created # I can also return the status code, like 200
+
+    else
+      render json: short_url.errors, status: :unprocessable_entity # Code error 422
+    end
+  end
+
+  private
+
+  def short_url_params
+    defaults = {title: ""}
+    params.require(:short_url).permit(:full_url, :title).reverse_merge(defaults)
   end
 
   def show
